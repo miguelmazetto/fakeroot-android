7c0f6f208daea4115cfa69c83d82200abf5d98d6
miguelmazetto <33629994+miguelmazetto@users.noreply.github.com>
2022-06-09 21:21:43 -0300

    Stuff for android-runlinux

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-SDIR=$PWD
+SDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
 export TOOLCHAIN=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64
 export ANDROID_USR=$TOOLCHAIN/sysroot/usr
 export API=21 # Set this to your minSdkVersion.
 
-rm -rf ./jniLibs; mkdir jniLibs
+cd $SDIR
 mkdir build
-
 ./bootstrap
-# echo "
-# #define LIBFAKEROOT_DEBUGGING" >> ./config.h.in
+
+RDIR=SDIR
+if [[ ! -z "$MMZ_ROOTFOLDER" ]]; then RDIR=$MMZ_ROOTFOLDER fi
 
 configure_android(){
 	export CFLAGS="-I$ANDROID_USR/include" # -fPIE
@@ -23,15 +23,22 @@ configure_android(){
 	export LD=$TOOLCHAIN/bin/ld
 	export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
 	export STRIP=$TOOLCHAIN/bin/llvm-strip
-	export JNIOUTDIR=$SDIR/jniLibs/$ARCH2
-	mkdir $JNIOUTDIR
+	JNIOUTDIR=$RDIR/jniLibs/$ARCH2
+	mkdir -p $JNIOUTDIR
 }
+
+greencol=$(tput setaf 46)
+defaultcol=$(tput sgr0)
 
 compile_fakeroot(){
 	cd $SDIR
 	mkdir build/$ARCH2; cd build/$ARCH2
+
+	printf "\n${greencol}Configuring fakeroot for $TARGET...\n\n${defaultcol}"
 	configure_android
 	../../configure --host $TARGET --with-ipc=tcp
+	
+	printf "\n${greencol}Compiling fakeroot for $TARGET...\n\n${defaultcol}"
 	make -j
 
 	cp faked $JNIOUTDIR/libfaked.so
